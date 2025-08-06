1d49c261dfc995c223cffa0c9e82f18a18d2710e
ShadowSurf-dev <ss.gd314159@gmail.com>
2025-08-06 16:42:59 -0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,26 +1,15 @@
-(async function () {
-  const base = "https://raw.githubusercontent.com/ShadowSurf-dev/Browser-Record/refs/heads/main/ui.html";
+(async () => {
+  const url = 'https://raw.githubusercontent.com/ShadowSurf-dev/Browser-Record/refs/heads/main/ui.html';
+  try {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error('Failed to load UI');
+    const html = await res.text();
 
-  const [htmlResp, jsResp, ffmpegResp] = await Promise.all([
-    fetch(base + "ui.html"),
-    fetch(base + "ui.js"),
-    fetch(base + "ffmpeg.min.js"),
-  ]);
-
-  const html = await htmlResp.text();
-  const js = await jsResp.text();
-  const ffmpeg = await ffmpegResp.text();
-
-  const fullHtml = html
-    .replace(
-      '</head>',
-      `<script>${ffmpeg}</script>\n<script>${js}</script>\n</head>`
-    )
-    .replace(/<script src=".*?ffmpeg.*?"><\/script>/, '') // remove old ffmpeg line if present
-    .replace(/<script src=".*?ui\.js.*?"><\/script>/, ''); // remove old ui.js line
-
-  const tab = window.open("about:blank", "_blank");
-  tab.document.open();
-  tab.document.write(fullHtml);
-  tab.document.close();
+    const win = window.open('about:blank', '_blank');
+    win.document.open();
+    win.document.write(html);
+    win.document.close();
+  } catch (err) {
+    alert('Loader error: ' + err.message);
+  }
 })();
